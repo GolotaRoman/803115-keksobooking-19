3be5c28c82aaddd7c33c00c493f7e6189f6a7906
GolotaRoman <roman.golota11@gmail.com>
2020-02-11 02:24:09 +0200

    3.2 метки на карте отображаются

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 'use strict';
 
 var OBJECTS_QUANTITY = 8;
-var ARRAY_AVATAR = 'img/avatars/user0' + Math.floor(Math.random() * 8) + '.png';
+
+var arrayAvatar = function () {
+  return 'img/avatars/user0' + (Math.floor(Math.random() * 8) + 1) + '.png';
+};
+
 var ARRAY_TITLE = 'some random title';
 var ARRAY_ADDRESS = 'location.x, location.y';
-var ARRAY_PRICE = Math.floor(Math.random() * 1000);
+
+var arrayPrice = function () {
+  return Math.floor(Math.random() * 1000);
+};
+
 var ARRAY_TYPE = ['flat', 'palace', 'house', 'bungalo'];
-var ARRAY_ROOMS = Math.floor((Math.random() * 3) + 1);
-var ARRAY_QUESTS = Math.floor((Math.random() * 4) + 1);
+var arrayRooms = function () {
+  return Math.floor((Math.random() * 3) + 1);
+};
+
+var arrayQuests = function () {
+  return Math.floor((Math.random() * 4) + 1);
+};
+
 var ARRAY_CHECKIN = ['12:00', '13:00', '14:00'];
 var ARRAY_CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi',
@@ -19,8 +33,15 @@ var ARRAY_DESCRIPTION = 'some random description';
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var LOCATION_X = Math.floor(Math.random() * 3000);
-var LOCATION_Y = Math.floor(Math.random() * 500 + 130);
+
+var locationX = function () {
+  return Math.floor(Math.random() * document.body.offsetWidth) + 1;
+};
+
+var locationY = function () {
+  return Math.floor(Math.random() * 500 + 130);
+};
+
 var PIN_SIZE = 40;
 
 var MAP = document.querySelector('.map');
@@ -28,7 +49,8 @@ var PIN_TEMPLATE = document.querySelector('#pin').content.querySelector('.map__p
 var PIN_WRAPPER = document.querySelector('.map__pins');
 
 var getRandomLength = function (data) { // Возвращает массив рандомной длины
-  data.length = Math.floor(Math.random() * data.length + 1);
+  var length = Math.floor(Math.random() * data.length + 1);
+  data.length = length;
   return data;
 };
 
@@ -44,15 +66,15 @@ var createArrayData = function () {
   for (var i = 0; i < OBJECTS_QUANTITY; i++) {
     array.push({
       'author': {
-        'avatar': ARRAY_AVATAR
+        'avatar': arrayAvatar()
       },
       'offer': {
         'title': ARRAY_TITLE,
         'address': ARRAY_ADDRESS,
-        'price': ARRAY_PRICE,
+        'price': arrayPrice(),
         'type': getRandomValue(ARRAY_TYPE),
-        'rooms': ARRAY_ROOMS,
-        'quests': ARRAY_QUESTS,
+        'rooms': arrayRooms(),
+        'quests': arrayQuests(),
         'checkin': getRandomValue(ARRAY_CHECKIN),
         'checkout': getRandomValue(ARRAY_CHECKOUT),
         'features': getRandomLength(FEATURES),
@@ -60,8 +82,8 @@ var createArrayData = function () {
         'photos': getRandomLength(PHOTOS)
       },
       'location': {
-        'x': LOCATION_X,
-        'y': LOCATION_Y
+        'x': locationX(),
+        'y': locationY()
       }
     });
   }
@@ -74,22 +96,30 @@ MAP.classList.remove('map--faded');
 
 // 3. Создание DOM элементов и заполнение их данными из массива.
 
-var getDescription = function () {
-  var pinElement = PIN_TEMPLATE.cloneNode(true);
-  createArrayData().forEeach(function (item) {
-    pinElement.querySelector('.pin__img').src = item.avatar;
-    pinElement.querySelector('.pin__img').alt = item.title;
-    pinElement.querySelector('.map-pin').style.left = item.location.x + (PIN_SIZE * Math.random());
-    pinElement.querySelector('.map-pin').style.top = item.location.y + (PIN_SIZE * Math.random());
-  });
-  return pinElement;
-};
-
 var fragment = document.createDocumentFragment();
-createArrayData().forEach(function (item, i) {
-  fragment.appendChild(getDescription(item, i));
-});
 
+function getPinElement(pinObject) {
+  var pinElement = PIN_TEMPLATE.cloneNode(true);
+
+  pinElement.querySelector('.pin__img').src = pinObject.author.avatar;
+  pinElement.querySelector('.pin__img').alt = pinObject.offer.title;
+  pinElement.style.left = pinObject.location.x + (PIN_SIZE * Math.random()) + 'px';
+  pinElement.style.top = pinObject.location.y + (PIN_SIZE * Math.random()) + 'px';
+
+  return pinElement;
+}
+
+function generatePins() {
+  var pinObjects = createArrayData();
+
+  pinObjects.forEach(function (item) {
+    var pinElement = getPinElement(item);
+
+    fragment.appendChild(pinElement);
+  });
+}
+
+generatePins();
 // 4. Отрисовка сгенерированных DOM-элементов в блок .map__pins.
 
 PIN_WRAPPER.appendChild(fragment);
