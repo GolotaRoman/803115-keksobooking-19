433768050aa305d0b825c2548a8912b7f025ace0
GolotaRoman <roman.golota11@gmail.com>
2020-02-16 21:50:02 +0200

    Код исправлен согласно рекомендаций

--- js/main.js
+++ js/main.js
@@ -1,89 +1,74 @@
 'use strict';
 
+// Константы
 var OBJECTS_QUANTITY = 8;
-
-var arrayAvatar = function () {
-  return 'img/avatars/user0' + (Math.floor(Math.random() * 8) + 1) + '.png';
-};
-
-var ARRAY_TITLE = 'some random title';
-var ARRAY_ADDRESS = 'location.x, location.y';
-
-var arrayPrice = function () {
-  return Math.floor(Math.random() * 1000);
-};
-
-var ARRAY_TYPE = ['flat', 'palace', 'house', 'bungalo'];
-var arrayRooms = function () {
-  return Math.floor((Math.random() * 3) + 1);
-};
-
-var arrayQuests = function () {
-  return Math.floor((Math.random() * 4) + 1);
-};
-
-var ARRAY_CHECKIN = ['12:00', '13:00', '14:00'];
-var ARRAY_CHECKOUT = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi',
-  'dishwasher',
-  'parking', 'washer',
-  'elevator',
-  'conditioner'];
-var ARRAY_DESCRIPTION = 'some random description';
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+var TITLE = 'some random title';
+var LOCATION = 'location.x, location.y';
+var DESCRIPTION = 'some random description';
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
-var locationX = function () {
-  return Math.floor(Math.random() * document.body.offsetWidth) + 1;
-};
-
-var locationY = function () {
-  return Math.floor(Math.random() * 500 + 130);
-};
-
-var PIN_SIZE = 40;
-
+var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+var HOUSING_TYPES = [
+  'flat',
+  'palace',
+  'house',
+  'bungalo'];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'];
 var MAP = document.querySelector('.map');
 var PIN_TEMPLATE = document.querySelector('#pin').content.querySelector('.map__pin');
 var PIN_WRAPPER = document.querySelector('.map__pins');
+var PIN_SIZE = 40;
+
+// Функции
+var generateRandomInt = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
 
 var getRandomLength = function (data) { // Возвращает массив рандомной длины
-  var length = Math.floor(Math.random() * data.length + 1);
-  data.length = length;
-  return data;
+  var copy = data.slice();
+  copy.length = generateRandomInt(0, copy.length - 1);
+  return copy;
 };
 
 var getRandomValue = function (value) { // Возвращает рандомный элемент массива
   return value[Math.floor(Math.random() * value.length)];
 };
 
+// Переменные
 var array = [];
 
 // 1.Функция для создания массива из сгенерированных объектов.
 
-var createArrayData = function () {
-  for (var i = 0; i < OBJECTS_QUANTITY; i++) {
+var createArrayData = function (i) {
+  for (i = 0; i < OBJECTS_QUANTITY; i++) {
     array.push({
       'author': {
-        'avatar': arrayAvatar()
+        'avatar': 'img/avatars/user0' + generateRandomInt(1, 8) + '.png'
       },
       'offer': {
-        'title': ARRAY_TITLE,
-        'address': ARRAY_ADDRESS,
-        'price': arrayPrice(),
-        'type': getRandomValue(ARRAY_TYPE),
-        'rooms': arrayRooms(),
-        'quests': arrayQuests(),
-        'checkin': getRandomValue(ARRAY_CHECKIN),
-        'checkout': getRandomValue(ARRAY_CHECKOUT),
+        'title': TITLE,
+        'address': LOCATION,
+        'price': generateRandomInt(1000, 10000),
+        'type': getRandomValue(HOUSING_TYPES),
+        'rooms': generateRandomInt(1, 4),
+        'quests': generateRandomInt(1, 5),
+        'checkin': getRandomValue(CHECKIN_CHECKOUT_TIME),
+        'checkout': getRandomValue(CHECKIN_CHECKOUT_TIME),
         'features': getRandomLength(FEATURES),
-        'description': ARRAY_DESCRIPTION,
+        'description': DESCRIPTION,
         'photos': getRandomLength(PHOTOS)
       },
       'location': {
-        'x': locationX(),
-        'y': locationY()
+        'x': generateRandomInt(1, document.body.offsetWidth),
+        'y': generateRandomInt(130, 630)
       }
     });
   }
